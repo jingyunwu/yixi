0ce2febdb47e9ae688eb1076ca8daccf7b7abb6c
jingyunwu <1029595155@qq.com>
2019-07-25 23:00:50 +0800

    add explore

--- src/components/pages/explore.vue
+++ src/components/pages/explore.vue
@@ -1,15 +1,40 @@
 <template>
   <div>
-    explore
+    <van-nav-bar
+      title="探索"
+      left-text="返回"
+      left-arrow
+      @click-left="onClickLeft"
+      fixed
+    />
+    <van-search placeholder="发现世界的另一面" v-model="value" shape="round" />
+   
   </div>
 </template>
 
 <script>
 export default {
-  name: 'explore'
+  name: 'explore',
+  data() {
+    return {
+      value: null
+    }
+  },
+  methods: {
+    onClickLeft() {
+      this.$router.push({path: '/home'})
+    },
+  }
 }
 </script>
 
-<style>
-
+<style lang="stylus" scoped>
+  .van-icon
+    color #000
+  .van-nav-bar__text
+    color #000
+  .van-search
+    margin-top 50px
+    .van-search__content
+      height 40px
 </style>
